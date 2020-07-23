ad6f37668f6143804825168d7dc0d0970a16cf78
Liam Moore <liam.moore.dev@gmail.com>
2020-07-23 20:29:57 +0000

    adds documentation to singly_number on a bit manip solution

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,24 +2,30 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# total runtime complexity n * (n - 1)/2 or just n
 def single_number(arr):
     # Your code here
 
     # create a map
     num_map = {}
 
-    # loop through the input array O(n)
+    # Loop through the input array O(n)
     for num in arr:
-        # increment occurences of given value to the corresponding key on the map
+        # If the number is already in map, delete that number the map
         if num in num_map:
-            num_map[num] += 1
+            del num_map[num]
+        # If the number isn't in the map add it to the map
         else:
             num_map[num] = 1
-    # loop through the map (O(n) - 1) /2
+    # At this point there is only one key, return that number
     for key in num_map:
-        # if count is one return the key
-        if num_map[key] == 1:
             return key
+    
+    # There is an alternative solution using the bitwise operator ^ that requires only O(1) space
+    # For now the above solution has the best possible speed, O(n) speed and O(n) space 
+    # Once you've finished some other problems come back to this one and use it to learn bitwise operations
+    # It definitely feels like this should be simpler for a computer to figure out
+    # Seems like bitwise operations are simpler ways of accomplishing very small binary logic
 
 
 if __name__ == '__main__':
