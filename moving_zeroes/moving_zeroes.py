11ccf53f9eaa703de9f32b829b0bf414b57082f9
Liam Moore <liam.moore.dev@gmail.com>
2020-07-23 21:50:13 +0000

    completes moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,8 +4,22 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    pass
+    # Create a variable to save the first zero, O(1) space
+    first_zero = None
+    # Loop until you find the first zero. O(n) speed
+    for index, value in enumerate(arr):
+        # If you find the first zero, save the location.
+        if value == 0 and first_zero is None:
+            first_zero = index
+        # If you have already found the first zero and you come across a nonzero.
+        elif first_zero is not None and value != 0:
+            # switch the nonzero value location with the first zero.
+            arr[index], arr[first_zero] = 0, value
+            # Increment the first zero to the next space, which will always be zero if it has reached this clause.
+            first_zero += 1
+    # Returns the same array with all zeroes moved to the right of all nonzero values
+    return arr
+        
 
 
 if __name__ == '__main__':
